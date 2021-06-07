ebf0e16feb9323f01c6d82bbb2b7fc56c73b14fe
Maciej Smolinski <contact@maciejsmolinski.com>
2021-06-08 01:24:54 +0200

    feat(typescript): convert syntax highlighter to TS

--- src/SyntaxHighlightedInput/SyntaxHighlightedInput.tsx
+++ src/SyntaxHighlightedInput/SyntaxHighlightedInput.tsx
@@ -1,23 +1,33 @@
 import "./styles.css";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, FC } from "react";
 import Token from "../Token";
 import { tokenize } from "../support/lang";
 
-function renderToken({ text, type }, index) {
+type Token = { text: string; type?: number };
+
+function renderToken({ text, type }: Token, index?: number) {
   return <Token text={text} type={type} key={text + index} />;
 }
 
-export default function SyntaxHighlightedInput({ code }) {
+type ComponentProps = { code: string };
+
+const SyntaxHighlightedInput: FC<ComponentProps> = ({ code }) => {
   const [value, setValue] = useState(code || "");
   const [scroll, setScroll] = useState({ x: 0, y: 0 });
-  const output = useRef(null);
+  const output = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+    if (!output.current) {
+      return;
+    }
+
     output.current.scrollLeft = scroll.x;
     output.current.scrollTop = scroll.y;
   }, [scroll.x, scroll.y]);
 
-  const tokens = tokenize(value).getAllTokens();
+  const tokens: Array<{ text: string; type: number }> = tokenize(
+    value
+  ).getAllTokens();
 
   return (
     <div className="SyntaxHighlightedInput">
@@ -28,15 +38,16 @@ export default function SyntaxHighlightedInput({ code }) {
           autoFocus={true}
           value={value}
           onChange={({ target }) => setValue(target.value)}
-          onScroll={({ target }) =>
-            setScroll({ x: target.scrollLeft, y: target.scrollTop })
-          }
+          onScroll={({ target }) => {
+            const element = target as HTMLAreaElement;
+
+            setScroll({
+              x: element.scrollLeft,
+              y: element.scrollTop,
+            });
+          }}
         />
-        <div
-          ref={output}
-          className="SyntaxHighlightedInput__output"
-          style={{ scrollLeft: scroll.x, scrollTop: scroll.y }}
-        >
+        <div ref={output} className="SyntaxHighlightedInput__output">
           {tokens.map(renderToken)}
           {tokens.length && tokens[tokens.length - 1].text.endsWith("\n")
             ? renderToken({ text: "\n" })
@@ -45,4 +56,6 @@ export default function SyntaxHighlightedInput({ code }) {
       </div>
     </div>
   );
-}
+};
+
+export default SyntaxHighlightedInput;
